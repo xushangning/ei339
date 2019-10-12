11012c7b671a4257af0d7e4314b3ecb5885062c5
Shining Xu <xushangning@sjtu.edu.cn>
2019-10-12 20:37:49 +0800

    Rewrite maze.py (2/2)
    Rewrite drawMap, now named as draw_map
    1. Figure enlarged to draw text
    2. Change the coding style
    3. Remove cluttering ticks

--- maze/maze.py
+++ maze/maze.py
@@ -31,39 +31,29 @@ class MazeProblem:
         Draw obstacles(1) as red rectangles. Draw path(0) as white rectangles.
         Draw starting point(2) and ending point(3) as circles.
         """
-        rowNum = len(self.map)
-        print(rowNum)
-        colNum = len(self.map[0])
-        print(colNum)
+        n_rows, n_cols = self.map.shape
+        plt.figure(figsize=(12.8, 9.6))
         ax = plt.subplot()
-        param = 1
-        for col in range(colNum):
-            for row in range(rowNum):
+        for row in range(n_rows):
+            for col in range(n_cols):
                 if self.map[row, col] == 2:
-                    circle = mpathes.Circle([param * col + param/2.0, param * row + param/2.0], param/2.0, color='g')
+                    circle = mpathes.Circle((col + 0.5, row + 0.5), 0.5, color='g')
                     ax.add_patch(circle)
                 elif self.map[row,col] == 3:
-                    circle = mpathes.Circle([param * col + param/2.0, param * row + param/2.0], param/2.0, color='y')
+                    circle = mpathes.Circle((col + 0.5, row + 0.5), 0.5, color='y')
                     ax.add_patch(circle)
                 elif self.map[row, col] == 1:
-                    rect = mpathes.Rectangle([param * col, param * row], param, param, color='r')
+                    rect = mpathes.Rectangle((col, row), 1, 1, color='r')
                     ax.add_patch(rect)
                 else:
-                    rect = mpathes.Rectangle([param * col, param * row], param, param, color='w')
+                    rect = mpathes.Rectangle((col, row), 1, 1, fill=False, edgecolor='0.75')
                     ax.add_patch(rect)
-        # Improve visualization
-        plt.xlim((0,colNum))
-        plt.ylim((0,rowNum))
-        my_x_ticks = np.arange(0,colNum+1, 1)
-        my_y_ticks = np.arange(0,rowNum+1, 1)
-        plt.xticks(my_x_ticks)
-        plt.yticks(my_y_ticks)
-        axx = plt.gca()
-        axx.xaxis.set_ticks_position('top')
-        axx.invert_yaxis()
-        plt.grid()
-        # Save maze image.
-        plt.savefig('./maze.jpg')
+        plt.xlim((0, n_cols))
+        plt.ylim((0, n_rows))
+        axis = plt.gca()
+        axis.xaxis.set_ticks_position('top')
+        axis.invert_yaxis()
+        plt.savefig('maze.jpg')
 
 
 if __name__ == "__main__":
