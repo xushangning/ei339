c4526029a300a48b5db8edc9c671fd0b0aa55b50
Shining Xu <xushangning@sjtu.edu.cn>
2019-10-13 23:35:19 +0800

    Use OO interface of matplotlib

--- maze/maze.py
+++ maze/maze.py
@@ -5,7 +5,8 @@ import matplotlib.patches as mpathes
 
 class MazeProblem:
     def __init__(self, maze_file=''):
-        self.map = self.load_map(maze_file)
+        self._map = self.load_map(maze_file)
+        self._fig = plt.figure(figsize=(12.8, 9.6))
 
     @staticmethod
     def load_map(file):
@@ -31,29 +32,27 @@ class MazeProblem:
         Draw obstacles(1) as red rectangles. Draw path(0) as white rectangles.
         Draw starting point(2) and ending point(3) as circles.
         """
-        n_rows, n_cols = self.map.shape
-        plt.figure(figsize=(12.8, 9.6))
-        ax = plt.subplot()
+        n_rows, n_cols = self._map.shape
+        ax = self._fig.subplots()
         for row in range(n_rows):
             for col in range(n_cols):
-                if self.map[row, col] == 2:
+                if self._map[row, col] == 2:
                     circle = mpathes.Circle((col + 0.5, row + 0.5), 0.5, color='g')
                     ax.add_patch(circle)
-                elif self.map[row,col] == 3:
+                elif self._map[row, col] == 3:
                     circle = mpathes.Circle((col + 0.5, row + 0.5), 0.5, color='y')
                     ax.add_patch(circle)
-                elif self.map[row, col] == 1:
+                elif self._map[row, col] == 1:
                     rect = mpathes.Rectangle((col, row), 1, 1, color='r')
                     ax.add_patch(rect)
                 else:
                     rect = mpathes.Rectangle((col, row), 1, 1, fill=False, edgecolor='0.75')
                     ax.add_patch(rect)
-        plt.xlim((0, n_cols))
-        plt.ylim((0, n_rows))
-        axis = plt.gca()
-        axis.xaxis.set_ticks_position('top')
-        axis.invert_yaxis()
-        plt.savefig('maze.jpg')
+        ax.set_xlim(0, n_cols)
+        ax.set_ylim(0, n_rows)
+        ax.xaxis.set_ticks_position('top')
+        ax.invert_yaxis()
+        self._fig.savefig('maze.jpg')
 
 
 if __name__ == "__main__":
